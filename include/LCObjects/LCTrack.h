21434c088d29ad2f89bb309bc0666df5c7c7e57d
Andre Sailer <andre.philippe.sailer@cern.ch>
2017-09-18 16:39:35 +0200

    LCTrack: implement Read and Write functions

--- include/LCObjects/LCTrack.h
+++ include/LCObjects/LCTrack.h
@@ -72,22 +72,16 @@ public:
      *
      *  @param  parameters the parameters to pass in constructor
      *  @param  fileReader the file reader, used to extract any additional parameters from file
-
-     * Doesn't work for vectors of trackStates
-
      */
-    pandora::StatusCode Read(Parameters &, pandora::FileReader &) const { return pandora::STATUS_CODE_SUCCESS; }
+    pandora::StatusCode Read(Parameters &, pandora::FileReader &) const;
 
     /**
      *  @brief  Persist any additional (derived class only) object parameters using the specified file writer
      *
      *  @param  pObject the address of the object to persist
      *  @param  fileWriter the file writer
-
-     * Doesn't work for vectors of trackStates
-
      */
-    pandora::StatusCode Write(const Object *const , pandora::FileWriter &) const { return pandora::STATUS_CODE_SUCCESS; }
+    pandora::StatusCode Write(const Object *const , pandora::FileWriter &) const;
 
     /**
      *  @brief  Create an object with the given parameters
@@ -138,59 +132,88 @@ inline pandora::StatusCode LCTrackFactory::Create(const Parameters &parameters, 
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-// inline pandora::StatusCode LCTrackFactory::Read(Parameters &parameters, pandora::FileReader &fileReader) const
-// {
-//     // ATTN: To receive this call-back must have already set file reader track factory to this factory
-//     LCTrackStates trackStates;
+inline pandora::StatusCode LCTrackFactory::Read(Parameters &parameters, pandora::FileReader &fileReader) const
+{
+    // ATTN: To receive this call-back must have already set file reader track factory to this factory
+    LCInputTrackStates trackStates;
+    int nTrackStates;
+    if (pandora::BINARY == fileReader.GetFileType())
+    {
+        pandora::BinaryFileReader &binaryFileReader(dynamic_cast<pandora::BinaryFileReader&>(fileReader));
+        PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, binaryFileReader.ReadVariable(nTrackStates));
+        for (int i = 0; i < nTrackStates; ++i)
+        {
+            pandora::TrackState trackState(0.0,0.0,0.0,0.0,0.0,0.0);
+            PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, binaryFileReader.ReadVariable(trackState));
+            trackStates.push_back( pandora::InputTrackState(trackState) );
+        }
+    }
+    else if (pandora::XML == fileReader.GetFileType())
+    {
+        pandora::XmlFileReader &xmlFileReader(dynamic_cast<pandora::XmlFileReader&>(fileReader));
+        PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, xmlFileReader.ReadVariable("NumberOfTrackStates", nTrackStates));
+        for (int i = 0; i < nTrackStates; ++i)
+        {
+            pandora::TrackState trackState(0.0,0.0,0.0,0.0,0.0,0.0);
+            std::stringstream trackStateName;
+            trackStateName << "TrackState" << i;
+            PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, xmlFileReader.ReadVariable(trackStateName.str(), trackState));
+            trackStates.push_back( pandora::InputTrackState(trackState) );
+        }
+    }
+    else
+    {
+        return pandora::STATUS_CODE_INVALID_PARAMETER;
+    }
 
-//     if (pandora::BINARY == fileReader.GetFileType())
-//     {
-//         pandora::BinaryFileReader &binaryFileReader(dynamic_cast<pandora::BinaryFileReader&>(fileReader));
-//         PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, binaryFileReader.ReadVariable(trackStates));
-//     }
-//     else if (pandora::XML == fileReader.GetFileType())
-//     {
-//         pandora::XmlFileReader &xmlFileReader(dynamic_cast<pandora::XmlFileReader&>(fileReader));
-//         PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, xmlFileReader.ReadVariable("TrackStates", trackStates));
-//     }
-//     else
-//     {
-//         return pandora::STATUS_CODE_INVALID_PARAMETER;
-//     }
+    LCTrackParameters &lcTrackParameters(dynamic_cast<LCTrackParameters&>(parameters));
+    lcTrackParameters.m_trackStates = trackStates;
 
-//     LCTrackParameters &lcTrackParameters(dynamic_cast<LCTrackParameters&>(parameters));
-//     lcTrackParameters.m_trackStates = trackStates;
-
-//     return pandora::STATUS_CODE_SUCCESS;
-// }
+    return pandora::STATUS_CODE_SUCCESS;
+}
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-// inline pandora::StatusCode LCTrackFactory::Write(const Object *const pObject, pandora::FileWriter &fileWriter) const
-// {
-//     // ATTN: To receive this call-back must have already set file writer track factory to this factory
-//     const LCTrack *const pLCTrack(dynamic_cast<const LCTrack*>(pObject));
+inline pandora::StatusCode LCTrackFactory::Write(const Object *const pObject, pandora::FileWriter &fileWriter) const
+{
+    // ATTN: To receive this call-back must have already set file writer track factory to this factory
+    const LCTrack *const pLCTrack(dynamic_cast<const LCTrack*>(pObject));
 
-//     if (!pLCTrack)
-//         return pandora::STATUS_CODE_INVALID_PARAMETER;
+    if (!pLCTrack)
+        return pandora::STATUS_CODE_INVALID_PARAMETER;
 
-//     if (pandora::BINARY == fileWriter.GetFileType())
-//     {
-//         pandora::BinaryFileWriter &binaryFileWriter(dynamic_cast<pandora::BinaryFileWriter&>(fileWriter));
-//         PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, binaryFileWriter.WriteVariable(pLCTrack->GetTrackStates()));
-//     }
-//     else if (pandora::XML == fileWriter.GetFileType())
-//     {
-//         pandora::XmlFileWriter &xmlFileWriter(dynamic_cast<pandora::XmlFileWriter&>(fileWriter));
-//         PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, xmlFileWriter.WriteVariable("TrackStates", pLCTrack->GetTrackStates()));
-//     }
-//     else
-//     {
-//         return pandora::STATUS_CODE_INVALID_PARAMETER;
-//     }
+    const LCTrackStates& trackStates = pLCTrack->GetTrackStates();
+    int nTrackStates = trackStates.size();
 
-//     return pandora::STATUS_CODE_SUCCESS;
-// }
+    if (pandora::BINARY == fileWriter.GetFileType())
+    {
+        pandora::BinaryFileWriter &binaryFileWriter(dynamic_cast<pandora::BinaryFileWriter&>(fileWriter));
+        PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, binaryFileWriter.WriteVariable(nTrackStates));
+        for (auto const& trackState : trackStates)
+        {
+            PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, binaryFileWriter.WriteVariable(trackState));
+        }
+    }
+    else if (pandora::XML == fileWriter.GetFileType())
+    {
+        pandora::XmlFileWriter &xmlFileWriter(dynamic_cast<pandora::XmlFileWriter&>(fileWriter));
+        PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, xmlFileWriter.WriteVariable("NumberOfTrackStates", nTrackStates));
+        int trackStateCounter=0;
+        for (auto const& trackState : trackStates)
+        {
+          std::stringstream trackStateName;
+          trackStateName << "TrackState" << trackStateCounter;
+          PANDORA_RETURN_RESULT_IF(pandora::STATUS_CODE_SUCCESS, !=, xmlFileWriter.WriteVariable(trackStateName.str(), trackState));
+          ++trackStateCounter;
+        }
+    }
+    else
+    {
+        return pandora::STATUS_CODE_INVALID_PARAMETER;
+    }
+
+    return pandora::STATUS_CODE_SUCCESS;
+}
 
 }//namespace
 
